2d909445a80d27f023b3206167d7c9529c98ebf4
cgchandan836-hue <cgchandan836@gmail.com>
2026-08-23 22:20:47 +0530

    Update CursedCape.java

--- src/main/java/me/cursedspider/cursedcape/CursedCape.java
+++ src/main/java/me/cursedspider/cursedcape/CursedCape.java
@@ -2,108 +2,77 @@ package me.cursedspider.cursedcape;
 
 import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.profile.PlayerProfile;
-import org.bukkit.profile.PlayerSkinPatch;
 
-public class CursedCape extends JavaPlugin implements Listener {
+public class CursedCape extends JavaPlugin {
 
     private NamespacedKey capeKey;
 
     @Override
     public void onEnable() {
-
         capeKey = new NamespacedKey(this, "capes/mycape");
 
-        getServer().getPluginManager().registerEvents(this, this);
+        getCommand("cape").setExecutor((sender, command, label, args) -> {
 
-        if (getCommand("cape") != null) {
+            if (!(sender instanceof Player player)) {
+                sender.sendMessage("Only players can use this command.");
+                return true;
+            }
 
-            getCommand("cape").setExecutor((sender, command, label, args) -> {
+            if (!player.hasPermission("cursedcape.use")) {
+                player.sendMessage("§cYou don't have permission.");
+                return true;
+            }
 
-                if (!(sender instanceof Player player)) {
-                    sender.sendMessage("Only players can use this command.");
-                    return true;
-                }
-
-                if (!player.hasPermission("cursedcape.use")) {
-                    player.sendMessage("§cYou don't have permission.");
-                    return true;
-                }
-
-                if (args.length == 0) {
-                    player.sendMessage("§e/cape set");
-                    player.sendMessage("§e/cape remove");
-                    return true;
-                }
-
-                if (args[0].equalsIgnoreCase("set")) {
-
-                    applyCape(player);
-
-                    player.sendMessage("§aCursedSpider Cape equipped! §7🕷️");
-
-                    return true;
-                }
-
-                if (args[0].equalsIgnoreCase("remove")) {
-
-                    removeCape(player);
-
-                    player.sendMessage("§aCursedSpider Cape removed.");
-
-                    return true;
-                }
-
+            if (args.length == 0) {
                 player.sendMessage("§e/cape set");
                 player.sendMessage("§e/cape remove");
-
                 return true;
-            });
-        }
+            }
+
+            if (args[0].equalsIgnoreCase("set")) {
+                applyCape(player);
+                player.sendMessage("§aCursedSpider Cape equipped! §7🕷");
+                return true;
+            }
+
+            if (args[0].equalsIgnoreCase("remove")) {
+                removeCape(player);
+                player.sendMessage("§aCursedSpider Cape removed.");
+                return true;
+            }
+
+            player.sendMessage("§e/cape set");
+            player.sendMessage("§e/cape remove");
+            return true;
+        });
 
         getLogger().info("CursedCape enabled!");
     }
 
     private void applyCape(Player player) {
-
         PlayerProfile profile = player.getPlayerProfile();
 
-        PlayerSkinPatch patch = profile.getSkinPatch();
-
+        var patch = profile.getSkinPatch();
         patch.setCapeTexturePatch(capeKey);
-
         profile.setSkinPatch(patch);
 
         player.setPlayerProfile(profile);
     }
 
     private void removeCape(Player player) {
-
         PlayerProfile profile = player.getPlayerProfile();
 
-        PlayerSkinPatch patch = profile.getSkinPatch();
-
+        var patch = profile.getSkinPatch();
         patch.setCapeTexturePatch(null);
-
         profile.setSkinPatch(patch);
 
         player.setPlayerProfile(profile);
     }
 
-    @EventHandler
-    public void onJoin(PlayerJoinEvent event) {
-
-        // Cape is not automatically equipped on join yet.
-        // /cape set will apply it.
-    }
-
     @Override
     public void onDisable() {
-
         getLogger().info("CursedCape disabled.");
     }
 }
